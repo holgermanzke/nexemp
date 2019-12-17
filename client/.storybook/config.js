edc509e6b5c7fdee521fe02044a8a83a87709b8d
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-12-17 16:34:36 +0100

    Add GlobalStyles, theming and font to all stories. (#36)

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,18 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import GlobalStyles from "../src/components/GlobalStyles";
+import { ThemeProvider } from "emotion-theming";
+import dark from "../src/themes/dark";
+
+//add GlobalStyles to all stories
+const GlobalStyleDecorator = storyFn => (
+  <ThemeProvider theme={dark}>
+    <GlobalStyles />
+    {storyFn()}
+  </ThemeProvider>
+);
+
+addDecorator(GlobalStyleDecorator);
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(require.context("../src/stories", true, /\.stories\.js$/), module);
