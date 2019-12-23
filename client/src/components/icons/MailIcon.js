8957c9848c939945e38c7920d1e5568c192c9a45
holgermanzke <manzkeholger@gmail.com>
2019-12-23 20:46:12 +0100

    Fix issue with class name in svg

--- client/src/components/icons/MailIcon.js
+++ client/src/components/icons/MailIcon.js
@@ -18,15 +18,10 @@ export default function MailIcon() {
     <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.238 21.4">
       <g transform="translate(-0.881 -4.5)">
         <Path
-          class="a"
           d="M5.3,6H23.7A2.307,2.307,0,0,1,26,8.3V22.1a2.307,2.307,0,0,1-2.3,2.3H5.3A2.307,2.307,0,0,1,3,22.1V8.3A2.307,2.307,0,0,1,5.3,6Z"
           transform="translate(0 0)"
         />
-        <path
-          class="a"
-          d="M26,9,14.5,19.5,3,9"
-          transform="translate(0 -1.244)"
-        />
+        <path d="M26,9,14.5,19.5,3,9" transform="translate(0 -1.244)" />
       </g>
     </Svg>
   );
