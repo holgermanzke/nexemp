3ccf4a5938cf43515299118cdc244839e9c77da5
holgermanzke <manzkeholger@gmail.com>
2019-12-27 20:09:12 +0100

    Add react router

--- client/src/components/icons/LogoutIcon.js
+++ client/src/components/icons/LogoutIcon.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
+const Button = styled(Link)``;
 const Svg = styled.svg`
   margin-top: 39px;
   margin-right: 25px;
@@ -13,17 +15,22 @@ const Logout = styled.path`
   :hover {
     fill: ${props => props.theme.colors.secondary};
   }
+  :active {
+    fill: ${props => props.theme.colors.primary};
+  }
 `;
 
 export default function LogoutIcon() {
   return (
-    <Svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="36"
-      height="31.5"
-      viewBox="0 0 36 31.5"
-    >
-      <Logout d="M13.5,0V4.5h18V27h-18v4.5H36V0ZM9,9,0,15.75,9,22.5V18H27V13.5H9Z" />
-    </Svg>
+    <Button to="/">
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36"
+        height="31.5"
+        viewBox="0 0 36 31.5"
+      >
+        <Logout d="M13.5,0V4.5h18V27h-18v4.5H36V0ZM9,9,0,15.75,9,22.5V18H27V13.5H9Z" />
+      </Svg>
+    </Button>
   );
 }
---
+++ client/src/pages/MyCandidates.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyCandidates() {
+  return <h1>Meine Kandidaten</h1>;
+}
