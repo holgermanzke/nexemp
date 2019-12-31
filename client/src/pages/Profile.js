e1e33a98c3442862e551e5dd0393254a604ee6a0
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-12-31 10:22:09 +0100

    Add api and proxy (#51)
    * Add api and proxy
    
    * Add data from database to candidate details
    
    * Fix comments on review

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,17 +3,41 @@ import CandidateDetails from "../components/CandidateDetails";
 import Separator from "../components/Separator";
 import ProfileDetails from "../components/ProfileDetails";
 import styled from "@emotion/styled";
+import { getCandidateDetails } from "../components/getCandidateDetails";
 
 const PageWrapper = styled.div`
   display: flex;
 `;
 
-export default function Profile() {
+export default function Profile(candId) {
+  const [profile, setProfile] = React.useState([]);
+
+  async function refreshProfile() {
+    const foundProfile = await getCandidateDetails(candId);
+    setProfile(foundProfile);
+  }
+
+  React.useEffect(() => {
+    refreshProfile();
+  });
   return (
-    <PageWrapper>
-      <CandidateDetails />
-      <Separator />
-      <ProfileDetails />
-    </PageWrapper>
+    <>
+      {profile.map(profile => {
+        return (
+          <PageWrapper key={profile.id}>
+            <CandidateDetails
+              candName={profile.first_name + " " + profile.last_name}
+              candProfession={profile.position_or_graduation}
+              candEMail={profile.mail}
+              candPhone={profile.sender_phone_first}
+              candStreet={profile.street + " " + profile.number}
+              candCity={profile.town}
+            />
+            <Separator />
+            <ProfileDetails />
+          </PageWrapper>
+        );
+      })}
+    </>
   );
 }
