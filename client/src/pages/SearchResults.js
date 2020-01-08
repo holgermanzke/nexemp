9824f00d3aa3a043292025ef03c462a7b6ddb907
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2020-01-08 17:24:12 +0100

    Create search result page with components (#69)

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { getSearchResults } from "../components/api/getSearchResults";
 import { useLocation } from "react-router-dom";
+import SingleSearchResult from "../components/searchresults/SingleSearchResult";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  margin-top: 75px;
+`;
 
 export default function SearchResults() {
   const [results, setResults] = React.useState([]);
@@ -9,19 +15,28 @@ export default function SearchResults() {
   React.useEffect(() => {
     async function refreshResults() {
       const foundResults = await getSearchResults(location);
-      console.log(foundResults);
       setResults(foundResults);
     }
     refreshResults();
   }, [location]);
-  console.log(location);
-  console.log(results);
   return (
     <>
+      <Container />
       {results.map(result => {
-        return result.first_name;
+        return (
+          <div key={result.id}>
+            <SingleSearchResult
+              profession={result.which_job}
+              experience={5}
+              skill1={result.position_or_graduation}
+              skill2={result.first_activity}
+              location={result.town}
+              age="34"
+              profileURL={`../profile/${result.id}`}
+            />
+          </div>
+        );
       })}
-      1234
     </>
   );
 }
