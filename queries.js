9d42a1d7de39bad60806e64cf414cb95296fddc1
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2020-01-08 17:21:51 +0100

    Add dotenv (#68)
    * Add dotenv
    
    * Add .env to gitignore

--- queries.js
+++ queries.js
@@ -1,9 +1,10 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 const pool = new Pool({
-  user: "holgermanzke",
-  host: "localhost",
-  database: "dbtest",
-  password: "neuefische",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
   port: 5432
 });
 
