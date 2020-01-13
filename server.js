80163b9bef180b8dc38af2ab2665d30e974832f7
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2020-01-13 12:13:25 +0100

    Add Heroku setup (#96)
    * Add heroku setup
    
    * Add app.json
    
    * Fix problems with storybook and node version

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3080;
+const path = require("path");
+const port = process.env.PORT || 3080;
 const db = require("./db");
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(express.json());
 
@@ -12,6 +16,10 @@ app.get("/", (request, response) => {
 app.get("/api/candidates", db.getCandidates);
 app.get("/api/candidates/:id", db.getCandidatesById);
 
+// Handle React routing, return all requests to React app
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
